0a174a28987ff379b4f94d8608b5651d919d0f72
seung.hwan.baek <46323118+elbicuderri@users.noreply.github.com>
2021-04-17 11:17:31 +0900

    Update train_lenet.py

--- train_lenet.py
+++ train_lenet.py
@@ -11,31 +11,31 @@ import numpy as np
 from torchsummary import summary
 from quan_model.lenet_quan import QuantLeNet
 
-# class QuantLeNet(Module):
-#     def __init__(self):
-#         super(QuantLeNet, self).__init__()
-#         self.quant_inp = QuantIdentity(bit_width=8)
-#         self.conv1 = QuantConv2d(3, 6, 5, weight_bit_width=4)
-#         self.relu1 = QuantReLU(bit_width=8)
-#         self.conv2 = QuantConv2d(6, 16, 5, weight_bit_width=4)
-#         self.relu2 = QuantReLU(bit_width=8)
-#         self.fc1   = QuantLinear(16*5*5, 120, bias=True, weight_bit_width=4)
-#         self.relu3 = QuantReLU(bit_width=8)
-#         self.fc2   = QuantLinear(120, 84, bias=True, weight_bit_width=4)
-#         self.relu4 = QuantReLU(bit_width=8)
-#         self.fc3   = QuantLinear(84, 10, bias=False, weight_bit_width=4)
+class QuantLeNet(Module):
+    def __init__(self):
+        super(QuantLeNet, self).__init__()
+        self.quant_inp = QuantIdentity(bit_width=8)
+        self.conv1 = QuantConv2d(3, 6, 5, weight_bit_width=4)
+        self.relu1 = QuantReLU(bit_width=8)
+        self.conv2 = QuantConv2d(6, 16, 5, weight_bit_width=4)
+        self.relu2 = QuantReLU(bit_width=8)
+        self.fc1   = QuantLinear(16*5*5, 120, bias=True, weight_bit_width=4)
+        self.relu3 = QuantReLU(bit_width=8)
+        self.fc2   = QuantLinear(120, 84, bias=True, weight_bit_width=4)
+        self.relu4 = QuantReLU(bit_width=8)
+        self.fc3   = QuantLinear(84, 10, bias=False, weight_bit_width=4)
 
-#     def forward(self, x):
-#         out = self.quant_inp(x)
-#         out = self.relu1(self.conv1(x))
-#         out = F.max_pool2d(out, 2)
-#         out = self.relu2(self.conv2(out))
-#         out = F.max_pool2d(out, 2)
-#         out = out.view(out.size(0), -1)
-#         out = self.relu3(self.fc1(out))
-#         out = self.relu4(self.fc2(out))
-#         out = self.fc3(out)
-#         return out
+    def forward(self, x):
+        out = self.quant_inp(x)
+        out = self.relu1(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = self.relu2(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = self.relu3(self.fc1(out))
+        out = self.relu4(self.fc2(out))
+        out = self.fc3(out)
+        return out
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
